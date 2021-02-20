059b910bb49730fd00f7bfcd742f7dc70d0aa6f2
laika <71929285+01laika@users.noreply.github.com>
2021-02-20 00:37:46 +0000

    Update status.py

--- status.py
+++ status.py
@@ -18,30 +18,35 @@ else:
     if config.get('token') == "token-here":
             print("did you put your token in the token.json file")
     else:
-        
-        status = [ #put whatever you want to change your status to in-between the quotation marks
-          "example",
-          "",
-          "",
-          "",
-          ""
-        ]
 
-        headers = {
-            'Authorization': token,
-            'Content-Type': 'application/json'
-        }
+        tokenv = {"Authorization": token}
+        valid = requests.get("https://discord.com/api/v8/users/@me/settings", headers=tokenv)
+        if valid.status_code == 401:
+            print("invalid token!")
+        else:
 
-        def main():
-            print("{+} Changing Status! {+}")
-            while True:
-                for each in status:
-                    status_data = ({'custom_status': {'text': each}})
-                    requests.patch(url, headers=headers, json=status_data)
-                    time.sleep(delay)
+            status = [ #put whatever you want to change your status to in-between the quotation marks
+            "example",
+            "example 1",
+            "",
+            ""
+            ]
+
+            headers = {
+                'Authorization': token,
+                'Content-Type': 'application/json'
+            }
+
+            def main():
+                print("{+} Changing Status! {+}")
+                while True:
+                    for each in status:
+                        status_data = ({'custom_status': {'text': each}})
+                        requests.patch(url, headers=headers, json=status_data)
+                        time.sleep(delay)
 
 
-        if __name__== "__main__": 
-            main() 
+            if __name__== "__main__": 
+                main() 
 
 
